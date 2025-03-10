d27c53d391ffd69815452cb686bdab62b835d1d7
Citlali Garcia <citlali.garcia.ponce@gmail.com>
2025-03-09 18:39:57 -0600

    first commit

---
+++ assets/js/alquiler.js
@@ -0,0 +1,126 @@
+const propiedades_alquiler = [
+    {
+    id: 1,
+    nombre: 'Apartamento en el centro de la ciudad',
+    src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+    descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+    ubicacion: '123 Main Street, Anytown, CA 91234',
+    habitaciones: '2 ',
+    baños:'2 ',
+    costo: '2.000',
+    smoke: false ,
+    pets: true,
+    },
+
+    {
+        id: 2,
+        nombre: ' Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue,Seaside Town, CA 56789',
+        habitaciones: '3 ',
+        baños:'3 ',
+        costo: ' 2.500',
+        smoke: true ,
+        pets: true,
+        },
+
+        {
+            id: 3,
+            nombre: 'Condominio moderno en zona residencial ',
+            src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+            descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+            ubicacion: '123 Main Street, Anytown, CA 91234',
+            habitaciones: '2 ',
+            baños:'2 ',
+            costo: '  2.200',
+            smoke: false ,
+            pets: false,
+            },
+            {
+                id: 4,
+                nombre: 'Hermosa residencia en venta dentro de exclusiva privada con vigilancia 24/7 y acceso controlado ',
+                src: 'https://propiedades.com/_next/image?url=https%3A%2F%2Fpropiedadescom.s3.amazonaws.com%2Ffiles%2Foriginal%2Fcalzada-de-las-aguilas-lomas-axomiatla-alvaro-obregon-df-cdmx-27080054-foto-01.jpg&w=2048&q=75',
+                descripcion: 'a privada cuenta con salón de fiestas, amplias zonas verdes con juegos infantiles, cancha de futbol rápido, pista para jogging y zona para meditar. ',
+                ubicacion: 'Calz de las Águilas, Colonia Lomas Axomiatla',
+                habitaciones: '4',
+                baños:'4 ',
+                costo: ' 5.000',
+                smoke: true ,
+                pets: true,
+                },  
+                {
+                    id: 5,
+                    nombre: 'Yo Soy Plenitud Luz Paz Armonía en la Naturaleza ',
+                    src: 'https://a0.muscache.com/im/pictures/372e8ae7-fccd-4536-af33-2722f952542a.jpg?im_w=1200&im_format=avif',
+                    descripcion: 'Relájate con toda la familia en este alojamiento donde la tranquilidad se respira. Plenitud, Paz, Armonía, en la Naturaleza. Con sus bellos Jardines, Alberca, Asador. ',
+                    ubicacion: 'villa en Lomas de Cocoyoc, México',
+                    habitaciones: '4 ',
+                    baños:'4.5 ',
+                    costo: '  4.000',
+                    smoke: false ,
+                    pets: true,
+                    },
+                    {
+                        id: 6,
+                        nombre: ' Hermosa casa en Cuernavaca Ficus',
+                        src: 'https://a0.muscache.com/im/pictures/hosting/Hosting-1123635427187757344/original/3484fec9-d62c-42ff-a36e-c793c93619e6.jpeg?im_w=1200&im_format=avif',
+                        descripcion: 'Diviértete con toda la familia en este alojamiento con estilo Minimalista, completamente nueva.',
+                        ubicacion: 'Cuernavaca, México',
+                        habitaciones: '4 ',
+                        baños:'3 ',
+                        costo: '  3.100',
+                        smoke: true ,
+                        pets: true,
+                        }
+    ]
+
+
+    const alquilerSection = document.getElementById('alquiler');
+    let propiedadesHTML = ""; 
+    
+    for (const propiedad of propiedades_alquiler) {
+        const propiedadTemplateString = `
+        
+            <div class="card">
+                <img
+                    src="${propiedad.src}"
+                    class="card-img-top"
+                    alt="Imagen del departamento"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">
+                        ${propiedad.nombre}
+                    </h5>
+                    <p class="card-text">
+                        ${propiedad.descripcion}
+                    </p>
+                    <p>
+                        <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                    </p>
+                    <p>
+                        <i class="fas fa-bed"></i> ${propiedad.habitaciones} <span> Habitaciones</span> <span> |</span>
+                        <i class="fas fa-bath"></i> ${propiedad.baños} <span>Baños</span>
+                    </p>
+                    <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo} </p>
+                    <p>
+                        <span class="${propiedad.smoke ? 'text-success' : 'text-danger'}">
+                            <i class="${propiedad.smoke ? 'fas fa-smoking' : 'fas fa-smoking-ban'}"></i>
+                            ${propiedad.smoke ? ' Permitido fumar' : ' No se permite fumar'}
+                        </span>
+                    </p>
+                    <p>
+                        <span class="${propiedad.pets ? 'text-success' : 'text-danger'}">
+                            <i class="${propiedad.pets ? 'fas fa-paw' : 'fa-solid fa-ban'}"></i>
+                            ${propiedad.pets ? ' Mascotas permitidas' : ' No se permiten mascotas'}
+                        </span>
+                    </p>
+                </div>
+            </div>
+     </div>
+        `;
+    
+        propiedadesHTML += propiedadTemplateString; 
+    }
+    
+    alquilerSection.innerHTML = propiedadesHTML;
---
+++ assets/js/venta.js
@@ -0,0 +1,126 @@
+const propiedades_venta = [
+    {
+    id: 1,
+    nombre: 'Apartamento de lujo en zona exclusiva',
+    src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+    descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+    ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+    habitaciones: '4 ',
+    baños:'4',
+    costo: '5.000',
+    smoke: false ,
+    pets: false,
+    },
+
+    {
+        id: 2,
+        nombre: 'Apartamento acogedor en la montaña',
+        src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+        descripcion: ' Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas.',
+        ubicacion: '789 Mountain Road,  Summit Peaks, CA 23456',
+        habitaciones: '2 ',
+        baños:'1 ',
+        costo: '1.200',
+        smoke: true ,
+        pets: true,  
+    },
+
+    {
+        id: 3,
+        nombre: 'Penthouse de lujo con terraza panorámica',
+        src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+        descripcion: ' Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares.',
+        ubicacion: '567 Skyline Avenue,  Skyview City, CA 56789',
+        habitaciones: '3 ',
+        baños:'3 ',
+        costo: '4.500',
+        smoke: false ,
+        pets: true,  
+    },
+    {
+        id: 4,
+        nombre: 'Casa Seminueva',
+        src: 'https://img10.naventcdn.com/avisos/resize/18/01/45/02/24/87/1200x1200/1516271916.jpg?isFirstImage=true',
+        descripcion: ' Casa residencial con 900m2 de Terreno y 1,200 m2.',
+        ubicacion: 'Alpes, Lomas de Chapultepec.',
+        habitaciones: '4 ',
+        baños:'4 ',
+        costo: '6.500',
+        smoke: true ,
+        pets: true,  
+    },
+    {
+        id: 5,
+        nombre: 'Casa con Jardin',
+        src: 'https://img10.naventcdn.com/avisos/resize/18/01/44/87/12/34/1200x1200/1496642637.jpg?isFirstImage=true',
+        descripcion: ' Casa en renta o venta en bosques de las lomas con gran jardín y vista panorámica.',
+        ubicacion: 'Bosque de Manzanos, Bosques de las Lomas, Cuajimalpa de Morelos',
+        habitaciones: '2 ',
+        baños:'2',
+        costo: '7.500',
+        smoke: false ,
+        pets: true,  
+    },
+    {
+        id: 6,
+        nombre: 'Recámara en Departamento Compartido',
+        src: 'https://publications-media.homierent.com/publications-media/4/667db32a42e936003a040264/WhatsApp_Image_2024-07-11_at_9.52.49_AM_hd.jpg?width=700&height=700&fit=inside',
+        descripcion: ' Recámara en departamento compartido con baño privado y completamente amueblado solo para mujeres, ubicado en Condesa Sur. ',
+        ubicacion: 'Condesa Sur',
+        habitaciones: '1 ',
+        baños:'1',
+        costo: '2.000',
+        smoke: false ,
+        pets: true,  
+    }
+    ]
+    
+      
+      const ventasSection = document.getElementById('ventas');
+let propiedadesHTML = ""; 
+
+for (const propiedad of propiedades_venta) {
+    const propiedadTemplateString = `
+    
+        <div class="card">
+            <img
+                src="${propiedad.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+            />
+            <div class="card-body">
+                <h5 class="card-title">
+                    ${propiedad.nombre}
+                </h5>
+                <p class="card-text">
+                    ${propiedad.descripcion}
+                </p>
+                <p>
+                    <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                </p>
+                <p>
+                    <i class="fas fa-bed"></i> ${propiedad.habitaciones} <span> Habitaciones</span> <span> |</span>
+                    <i class="fas fa-bath"></i> ${propiedad.baños} <span>Baños</span>
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo} </p>
+                <p>
+                    <span class="${propiedad.smoke ? 'text-success' : 'text-danger'}">
+                        <i class="${propiedad.smoke ? 'fas fa-smoking' : 'fas fa-smoking-ban'}"></i>
+                        ${propiedad.smoke ? ' Permitido fumar' : ' No se permite fumar'}
+                    </span>
+                </p>
+                <p>
+                    <span class="${propiedad.pets ? 'text-success' : 'text-danger'}">
+                        <i class="${propiedad.pets ? 'fas fa-paw' : 'fa-solid fa-ban'}"></i>
+                        ${propiedad.pets ? ' Mascotas permitidas' : ' No se permiten mascotas'}
+                    </span>
+                </p>
+            </div>
+        </div>
+   </div>
+    `;
+
+    propiedadesHTML += propiedadTemplateString; 
+}
+
+ventasSection.innerHTML = propiedadesHTML;
